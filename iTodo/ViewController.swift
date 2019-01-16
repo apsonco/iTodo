3d8f0c8c11fffeaf32687a847b09415f3c9ec219
pavel.uh@gmail.com <pavel.uh@gmail.com>
2019-01-16 08:26:13 +0300

    Initial Commit

---
+++ iTodo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTodo
+//
+//  Created by Pavel on 1/16/19.
+//  Copyright © 2019 Pavel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
